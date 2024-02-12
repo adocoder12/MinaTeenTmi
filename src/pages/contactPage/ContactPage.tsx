4bab51b248e6a452f158e2e50b705a88b27ec508
adocoder12 <adonay.jadal@gmail.com>
2024-02-12 14:42:08 +0100

    fixing bugs

--- src/pages/contactPage/ContactPage.tsx
+++ src/pages/contactPage/ContactPage.tsx
@@ -6,17 +6,19 @@ import TopBanner from "@/components/topBanner/TopBanner";
 
 export default function ContactPage() {
   return (
-    <div className={style["contact__page__container"]}>
-      <TopBanner
-        title="Ota meihin yhteyttä"
-        text="Voidaksemme tarjota asiakkaillemme parasta palvelua, pyydämme
+    <>
+      <div className={style["contact__page__container"]}>
+        <TopBanner
+          title="Ota meihin yhteyttä"
+          text="Voidaksemme tarjota asiakkaillemme parasta palvelua, pyydämme
           täyttämään oheisen yhteydenottolomakkeen. Vastaamme viestiisi
           mahdollisimman pian."
-      />
-      <div className={style["contact__page__wrapper"]}>
-        <Form />
-        <ContactInfo />
+        />
+        <div className={style["contact__page__wrapper"]}>
+          <Form />
+          <ContactInfo />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
