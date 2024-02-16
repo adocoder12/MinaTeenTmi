500e8a0f5a88895322b73b9eb0a28eadd37884e1
adocoder12 <adonay.jadal@gmail.com>
2024-02-16 16:24:41 +0100

    fixing bugs css working responsive

--- src/components/contactInfo/ContactInfo.tsx
+++ src/components/contactInfo/ContactInfo.tsx
@@ -5,35 +5,46 @@ import { FaRegEnvelope } from "react-icons/fa";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
 export default function ContactInfo() {
+  const cotactInfo = [
+    {
+      icon: FaMapMarkerAlt,
+      title: "Y-tunnus",
+      text: "1231235234",
+    },
+    {
+      icon: FaRegUser,
+      title: "Yhteyshenkilö",
+      text: "Eduardo Cipriani",
+    },
+  ];
   return (
     <div className={style["contactInfo__container"]}>
-      <div className={style["contactInfo__wrapper"]}>
-        <div className={style["contactIcon__wrapper"]}>
-          <FaMapMarkerAlt />
-          <h2 className={style.IconTitle}>Y-tunnus</h2>
+      {cotactInfo.map((info) => (
+        <div className={style["contactInfo__wrapper"]}>
+          <div className={style["contactIcon__wrapper"]}>
+            <info.icon />
+            <h2 className={style.IconTitle}>{info.title}</h2>
+          </div>
+          <span className={style.IconInfo}> {info.text}</span>
         </div>
-        <span className={style.IconInfo}> yritys365 2959203-8</span>
-      </div>
+      ))}
       <div className={style["contactInfo__wrapper"]}>
         <div className={style["contactIcon__wrapper"]}>
           <FaPhoneAlt />
           <h2 className={style.IconTitle}>Puhelinnumero</h2>
         </div>
-        <span className={style.IconInfo}> +358400555555</span>
-      </div>
-      <div className={style["contactInfo__wrapper"]}>
-        <div className={style["contactIcon__wrapper"]}>
-          <FaRegUser />
-          <h2 className={style.IconTitle}>Yhteyshenkilö</h2>
-        </div>
-        <span className={style.IconInfo}> Eduardo Cipriani</span>
+        <a href="tel:+358555232352" className={style.IconInfo}>
+          +358555232352
+        </a>
       </div>
       <div className={style["contactInfo__wrapper"]}>
         <div className={style["contactIcon__wrapper"]}>
           <FaRegEnvelope />
           <h2 className={style.IconTitle}>Sähköpostiosoite</h2>
         </div>
-        <span className={style.IconInfo}> EduardoCipriani@mail.com</span>
+        <a href="mailto:minateentmi@gmail.com" className={style.IconInfo}>
+          EduardoCipriani@mail.com
+        </a>
       </div>
     </div>
   );
