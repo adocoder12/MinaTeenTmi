e24e9dc3eb303f04ec69d7bee632188f3c605ab1
adocoder12 <adonay.jadal@gmail.com>
2024-03-01 12:27:01 +0100

    adding service info

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,4 +1,7 @@
-import img_service from "@assets/servicesImg/Service_background_img.jpeg";
+import snow_work from "@assets/servicesImg/Snow_work.jpg";
+import garden_work from "@assets/servicesImg/garden-work.jpeg";
+import moving_work from "@assets/servicesImg/moving-service.jpg";
+import Remontti_work from "@assets/servicesImg/working-by-the-renovation.jpg";
 
 const service = [
   {
@@ -6,7 +9,7 @@ const service = [
     title: "Talvikunnossapito",
     description:
       "Käsilumityöt kertaluonteisesti tai koko talvikauden kestävä sopimusasiakkuus.",
-    img: img_service,
+    img: snow_work,
     content: `Avustamme kiinteistösi käsilumitöissä tarvitsemallasi laajuudella, joko kertakäynteinä tai koko talvikauden kattavalla sopimuksella. Koko talvikauden sopimukselle edullisempi tuntiveloitus 45€/h. `,
     price: " Käsilumityöt minimiveloitus on 25.00€ / 30 min",
   },
@@ -15,21 +18,21 @@ const service = [
     title: "Viheralueet",
     description:
       "Kaikki vihertyöt pensaista nurmikonhoitoon ja istutuksiin. Räätälöidään palvelukokonaisuus tai kertaluonteinen palvelu.",
-    img: img_service,
+    img: garden_work,
   },
   {
     id: 3,
     title: "Erilaiset asunnon remonttiprojektit",
     description:
       "Jos aiot remontoida asunnon, voit aloittaa useita erilaisia projekteja. Riippumatta siitä, aiotko tehdä peruskorjausta vai vain pintaremonttia, voit valita useista erilaisista projekteista. Olemme koonneet yhteen yleiskatsauksen asunnon useimpien huoneiden remonttiprojekteista.",
-    img: img_service,
+    img: Remontti_work,
   },
   {
     id: 4,
     title: "Siivous",
     description:
       "Olemme erikoistuneet muuttosiivoukseen. Siivoamme muuttokohteen ennen muuttoa ja teemme loppusiivouksen edelliseen asuntoon. Pyydä tarjous!.",
-    img: img_service,
+    img: moving_work,
     content:
       "Teemme myös koti ja rappusiivouksia asiakkaan toiveiden mukaan. Käytämme kohteissa sopimuksen mukaan, joko asiakkaan välineitä tai tuomme omat.",
     price: `Edullisempi tuntiveloitus 39,00€/h Säännölliselle siivoukselle kotisiivoukselle. Minimiveloitus on 2 tuntia, jonka jälkeen 30 min jaksoissa.`,
